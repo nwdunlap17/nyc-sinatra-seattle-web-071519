f24f8dd40ec354e2919f26491d062249e61d1695
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-08 15:27:58 -0700

    stuff

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,7 +10,17 @@ class FiguresController < ApplicationController
   end
 
   post '/figures/new' do
-    @new_figure= Title.new(params[:name])
+    binding.pry
+    @new_figure= Figure.create(params[:figure])
+    if params[:title][:name] != ""
+      @new_title = Title.create(params[:title])
+    end
+    if params[:landmark][:name] != ""
+      @new_landmark = Title.new
+      @new_landmark.name = params[:landmark][:name]
+      @new_landmark.year_completed = params[:landmark][:year]
+      @new_landmark.save
+    end
   end
 
 
